dc73a4604a16a4832ba76a539ad892530a2c07b5
topbun <akimov1712@mail.ru>
2024-06-20 13:55:56 +0300

    Colors added to constants

---
+++ composeApp/src/commonMain/kotlin/ru/topbun/cherry_tip/presentation/ui/Colors.kt
@@ -0,0 +1,26 @@
+package ru.topbun.cherry_tip.presentation.ui
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+
+    val White = Color(255, 255 ,255)
+    val Black = Color(32, 32 ,32)
+
+    val GrayLight = Color(153, 153 ,153)
+    val GrayDark = Color(88, 96 ,113)
+
+    val Purple = Color(114, 101 ,227)
+    val PurpleTransparent = Color(86, 75 ,181).copy(alpha = 0.8f)
+    val PurpleBackground = Color(237, 241 ,247)
+
+    val GreenLight = Color(81, 241 ,87)
+    val GreenDark = Color(101, 227 ,136)
+
+    val Red = Color(227, 101 ,101)
+
+    val Blue = Color(81, 135 ,241)
+
+    val Yellow = Color(241, 187 ,81)
+
+}
