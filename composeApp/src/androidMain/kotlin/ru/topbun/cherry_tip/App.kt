25431ea73c1b382d05d9e25f32d30979400a1f45
akimov1712 <akimov1712@mail.ru>
2024-10-14 00:26:30 +0300

    [feat]: implemented notifications to remind you to drink water

--- composeApp/src/androidMain/kotlin/ru/topbun/cherry_tip/App.kt
+++ composeApp/src/androidMain/kotlin/ru/topbun/cherry_tip/App.kt
@@ -1,6 +1,10 @@
 package ru.topbun.cherry_tip
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.compose.ui.graphics.Color
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.AppMetricaConfig
 import org.koin.android.ext.koin.androidContext
@@ -8,6 +12,8 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import ru.topbun.cherry_tip.di.appModule
 import ru.topbun.cherry_tip.utills.Const
+import java.util.Calendar
+
 
 class App: Application(){
 
@@ -18,7 +24,26 @@ class App: Application(){
             androidLogger()
             modules(appModule)
         }
+        setDailyAlarm()
         val config = AppMetricaConfig.newConfigBuilder(Const.APP_METRIC_API_KEY).build()
         AppMetrica.activate(this, config)
     }
+
+    private fun setDailyAlarm(){
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = NotifyWaterReceiver.newIntent(this)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 12)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+    }
+
 }
---
+++ composeApp/src/androidMain/kotlin/ru/topbun/cherry_tip/NotifyWaterReceiver.kt
@@ -0,0 +1,61 @@
+package ru.topbun.cherry_tip
+
+import android.app.AlarmManager
+import android.app.Application.ALARM_SERVICE
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import cherrytip.composeapp.generated.resources.Res
+import cherrytip.composeapp.generated.resources.reminder_drink_water_descr
+import cherrytip.composeapp.generated.resources.reminder_drink_water_title
+import coil3.compose.AsyncImagePainter.State.Empty.painter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.getString
+
+class NotifyWaterReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        CoroutineScope(Dispatchers.IO).launch {
+            context?.let {
+                val notificationManager = getSystemService(it, NotificationManager::class.java) as NotificationManager
+                createNotificationChannel(notificationManager)
+                val notification = NotificationCompat.Builder(it, CHANNEL_ID)
+                    .setContentTitle(getString(Res.string.reminder_drink_water_title))
+                    .setContentText(getString(Res.string.reminder_drink_water_descr))
+                    .setSmallIcon(R.drawable.ic_water)
+                    .build()
+                notificationManager.notify(NOTIFICATION_ID, notification)
+            }
+        }
+
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "reminder_water_id"
+        private const val CHANNEL_NAME = "reminder_water"
+        private const val NOTIFICATION_ID = 1
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, NotifyWaterReceiver::class.java)
+        }
+    }
+
+}
